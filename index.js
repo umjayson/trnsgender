59e3838235e197aa6353f0ec2df343c2613c45d6
umjayson <jaysonownsthis@gmail.com>
2024-10-07 20:26:43 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jayson here xd</title>
+
+    <meta property="og:title" content="trnsgender.lol" />
+    <meta property="og:image" content="https://images-ext-1.discordapp.net/external/5I54CWKf9K-7h3KqwRQMhe7KYKe92UgJrIb9LqdO9bw/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1277553981408677980/d91597284c7790dcebef36aa8d1da497.webp?format=webp&width=676&height=676" />
+    <meta property="og:url" content="https://trnsgender.lol" />
+    <meta name="twitter:card" content="summary_large_image">
+   <script disable-devtool-auto="" src="https://cdn.jsdelivr.net/npm/disable-devtool"></script>
+  
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: monospace;
+            text-align: center;
+            padding: 50px;
+        }
+        .content {
+            margin-top: 20%;
+        }
+        p {
+            font-size: 18px;
+            margin: 10px;
+        }
+        a {
+            color: lightblue;
+            text-decoration: none;
+        }
+        .folder {
+            display: inline-block;
+            margin-left: 10px;
+            color: yellow;
+        }
+        .icon {
+            display: inline-block;
+            margin-left: 10px;
+            color: grey;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <p>welcome to jayson's site</p>
+        <p>idk what to type here,<br> not a lot going on</p>
+        <p><a href="http://ilovemoney.lol/jayson">ilovemoney.lol/jayson</a> <span class="folder">📁</span> <span class="icon">👤</span></p>
+    </div>
+</body>
+</html>
